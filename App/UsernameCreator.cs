8c0dfe7aafdb832a49b8e77b9b88ab4b731aff9c
Luis Henrique Pacheco Paim <luispp@dbserver.com.br>
2017-03-07 14:19:05 -0300

    Coding the main class of the application

---
+++ App/UsernameCreator.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+public class UsernameFactory
+{
+    private IEnumerable<IFormatRule> Rules { get; set; }
+    private IEnumerable<IValidation> Validations { get; set; }
+    private string FullName  {get; set; }
+
+    public UsernameFactory(string fullName, IEnumerable<IFormatRule> rules, IEnumerable<IValidation> validations)
+    {
+        this.FullName = fullName;
+        this.Rules = rules;
+        this.Validations = validations;
+    }
+
+    public string Create() 
+    {
+        foreach(var rule in Rules)
+        {            
+            string userName = rule.Test(this.FullName);
+            bool isApropriated = true;
+            foreach(var val in Validations)
+            {
+                Console.WriteLine("{0}, {1}, {2}", userName, rule.GetType().FullName, val.GetType().FullName);
+                bool isValid = val.Validate(userName);
+                if(!isValid) 
+                {
+                    Console.WriteLine("{0} filed at {1} test", userName, val.GetType().FullName);
+                    isApropriated = isValid;
+                }
+            }            
+            if(isApropriated) {
+                return userName;
+            } 
+        }
+        throw new Exception("Unable to create a username");
+    }
+}
